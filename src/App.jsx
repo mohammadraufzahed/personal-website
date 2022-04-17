669b761b9448189d9eaf8955a7a9a7e19962b620
Mohammad <mohammad.bagheri24680@gmail.com>
2022-04-18 00:17:42 +0430

    added Contac_me to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { AboutMe } from "./Components/AboutMe/AboutMe";
+import { Contact_Me } from "./Components/Contact_Me/Contact_Me";
 import { Main } from "./Components/Main/Main";
 import { MyPortfolio } from "./Components/MyPortfolio/MyPortfolio";
 import { MySkills } from "./Components/MySkills/MySkills";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<AboutMe />} />
         <Route path="/skills" element={<MySkills />} />
         <Route path="/portfolio" element={<MyPortfolio />} />
+        <Route path="/contact" element={<Contact_Me />} />
         <Route path="/seemore" element={<Portfolio_SeeMore />} />
       </Routes>
     </>
